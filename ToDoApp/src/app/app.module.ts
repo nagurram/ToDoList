90b990d1fec3c5eda3f6ed6c84ee0f3b2f80e49f
Narendranath <naren.7090@outlook.com>
2020-05-01 16:04:27 +0530

    wip
    wip

--- ToDoApp/src/app/app.module.ts
+++ ToDoApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
@@ -12,7 +12,7 @@ import { ToDoListComponent } from './to-do-list/to-do-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ToDoApp/src/app/to-do-list/to-do-list.component.ts
+++ ToDoApp/src/app/to-do-list/to-do-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoModel } from '../Model/TodoModel';
 import { DatePipe } from '@angular/common';
+import { FormsModule} from '@angular/forms';  
 
 @Component({
   selector: 'app-to-do-list',
@@ -8,10 +9,31 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./to-do-list.component.css']
 })
 export class ToDoListComponent implements OnInit {
-
+  todomodel: TodoModel;
+  showfull:boolean;
   constructor() { }
   todolist: TodoModel[] = new Array();
   ngOnInit(): void {
+    this.showfull=false;
   }
+  openAddtodoModal( ) {
+    this.showfull=true;
+    this.additems();
+}
+CancelItem()
+{
+  this.showfull=false;
+}
 
+saveItem()
+{
+
+  this.todolist.push(this.todomodel );
+  this.showfull=false;
+}
+additems() {
+  this.todomodel = new TodoModel();
+  this.todomodel.TodoId =this.todolist.length+1;
+  this.todomodel.IsActive = true;
+}
 }
